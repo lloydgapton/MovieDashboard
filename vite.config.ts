ee6997b3e88e92f35af12cfff19c6ad9450fe4db
lloydgapton <lloydgapton123@gmail.com>
2025-04-06 16:10:38 -0700

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    modules: {
+      scopeBehaviour: 'local', // Ensures that your CSS modules are scoped locally to your components
+    },
+    }
+  })
