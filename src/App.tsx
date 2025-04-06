6a2b73292ba9868f0967d38c84c92d3a3ca7276e
lloydgapton <lloydgapton123@gmail.com>
2025-04-05 22:44:31 -0700

    implement StatsSection with genre-based bar chart, and installed recharts modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,6 @@
 import ErrorBoundary from "./components/ErrorBoundary";
 import SearchBar from "./components/SearchBar";
+import MovieList from "./components/MovieList";
+import MovieForm from "./components/MovieForm";
+import MovieCard from "./components/MovieCard";
+import StatsSection from "./components/StatsSection";
